832ad20a84ef4b81c97736d5842fc2f3e312031b
El rey demonio <sebastian.zorrilla@ontop.com.co>
2022-01-04 15:44:30 -0500

    Actualizado

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,40 @@
 import Navbar from "./Navbar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+const Layout = ({ children }) => {
+  const router = useRouter();
 
-const Layout = ({ children }) => (
-  <>
-    <Navbar />
-    <main className="container py-4">{children}</main>
-  </>
-);
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(url);
+      NProgress.start();
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    router.events.on("routeChangeComplete", () => NProgress.done());
+
+    router.events.on("routeChangeError", () => nProgress.done());
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <main className="container py-4">{children}</main>
+      <footer className="bg-dark text-light text-center">
+        <div className="container p-4">
+          <h1>&copy; Ryan Ray Portfolio</h1>
+          <p>2000 - {new Date().getFullYear()}</p>
+          <p>All rights Reserved.</p>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import { skills, experiences } from "../profile";
+import { skills, experiences, projects } from "../profile";
 import Link from "next/link";
 
 export default function Home() {
@@ -76,6 +76,45 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <section>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-body bg-dark">
+              <div className="row">
+                <div className="col-md-12 my-2">
+                  <h1 className="text-center text-light">Portfolio</h1>
+                </div>
+                {projects.map(({ name, description, image }, index) => (
+                  <div className="col-md-4 p-2" key={index}>
+                    <div className="card h-100">
+                      <div className="overflow">
+                        <img
+                          src={`/${image}`}
+                          alt=""
+                          className="card-img-top"
+                        />
+                      </div>
+                      <div className="card-body">
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <a href="#!">Know More</a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+                <div className="col-md-12 mt-4">
+                  <div className="text-center">
+                    <Link href="/portfolio">
+                      <a className="btn btn-outline-light">More Projects</a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
